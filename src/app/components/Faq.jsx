da03d868c6d59d0b279e28c3947c6d43636df254
sreeram051 <sreeram8122@gmail.com>
2024-06-03 16:27:43 +0530

    first commit

--- src/app/components/Faq.jsx
+++ src/app/components/Faq.jsx
@@ -17,7 +17,7 @@ const Faq = () => {
         The Best Financial Accounting App Ever!
       </h1>
       <p className='font-clash font-normal text-[18px] leading-[28px] text-white text-justify'>
-        "Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".
+        Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris.
       </p>
     </div>
     <div className='rounded-md mb-[20px] w-full lg:w-[530px] h-auto px-[31px] py-[31px]'>
@@ -25,7 +25,7 @@ const Faq = () => {
         The Best Financial Accounting App Ever!
       </h1>
       <p className='font-clash font-normal text-[18px] leading-[28px] text-justify'>
-        "Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".
+        Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris.
       </p>
     </div>
   </div>
@@ -35,7 +35,7 @@ const Faq = () => {
         The Best Financial Accounting App Ever!
       </h1>
       <p className='font-clash font-normal text-[18px] leading-[28px] text-justify'>
-        "Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".
+        Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris.
       </p>
     </div>
     <div className='rounded-md bg-main mb-[20px] w-full lg:w-[530px] h-auto px-[31px] py-[31px]'>
@@ -43,7 +43,7 @@ const Faq = () => {
         The Best Financial Accounting App Ever!
       </h1>
       <p className='font-clash font-normal text-[18px] leading-[28px] text-white text-justify'>
-        "Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".
+        Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris.
       </p>
     </div>
   </div>
@@ -53,7 +53,7 @@ const Faq = () => {
         The Best Financial Accounting App Ever!
       </h1>
       <p className='font-clash font-normal text-[18px] leading-[28px] text-white text-justify'>
-        "Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".
+        Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris.
       </p>
     </div>
     <div className='rounded-md mb-[20px] w-full lg:w-[530px] h-auto px-[31px] py-[31px]'>
@@ -61,58 +61,12 @@ const Faq = () => {
         The Best Financial Accounting App Ever!
       </h1>
       <p className='font-clash font-normal text-[18px] leading-[28px] text-justify'>
-        "Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".
+        Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris.
       </p>
     </div>
   </div>
 </div>
 
-//   return (
-//     <div className=' 2xl:mx-[175px] mb-[140px]'>
-//             <div className='mx-[175px]'>
-//                 <p className=' font-clash text-main leading-[28px] text-[18px] tracking-wide font-medium uppercase mb-1'>faq</p>
-//                 <h1 className='font-clash font-bold leading-[48px] text-[48px] w-[497px] h-[96px] mb-[40px]'>Frequently asked questions</h1>
-//             </div>
-//             <div className=' flex space-x-4 mx-[175px]'>
-//                     <div className=' rounded-md bg-main mb-[20px] w-[530px] h-[180px] px-[31px] py-[31px]'>
-//                         <h1 className=' text-white w-[400px] h-[56px] font-clash font-semibold text-[28px] leading-[28px] mb-2'> The Best Financial Accounting App Ever!</h1>
-//                         <p className=' w-[466px] h-[56px] font-clash font-normal text-[18px] leading-[28px] text-white text-justify'>"Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".</p>
-//                     </div>
-//                     <div>
-//                         <div className=' mb-[20px] w-[530px] h-[180px] px-[31px] py-[31px]'>
-//                               <h1 className='  w-[400px] h-[56px] font-clash font-semibold text-[28px] leading-[28px] mb-2'> The Best Financial Accounting App Ever!</h1>
-//                         <p className=' w-[466px] h-[56px] font-clash font-normal text-[18px] leading-[28px] text-justify'>"Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".</p>
-//                         </div>
-                  
-//                     </div>
-//             </div>
-//             <div className=' flex space-x-4 mx-[175px]'>
-//                     <div className=' rounded-md  mb-[20px] w-[530px] h-[180px] px-[31px] py-[31px]'>
-//                         <h1 className='  w-[400px] h-[56px] font-clash font-semibold text-[28px] leading-[28px] mb-2'> The Best Financial Accounting App Ever!</h1>
-//                         <p className=' w-[466px] h-[56px] font-clash font-normal text-[18px] leading-[28px]  text-justify'>"Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".</p>
-//                     </div>
-//                     <div>
-//                         <div className=' bg-main mb-[20px] rounded-md  w-[530px] h-[180px] px-[31px] py-[31px]'>
-//                               <h1 className='text-white  w-[400px] h-[56px] font-clash font-semibold text-[28px] leading-[28px] mb-2'> The Best Financial Accounting App Ever!</h1>
-//                         <p className=' w-[466px] h-[56px] font-clash font-normal text-white text-[18px] leading-[28px] text-justify'>"Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".</p>
-//                         </div>
-                  
-//                     </div>
-//             </div>
-//             <div className=' flex space-x-4 mx-[175px]'>
-//                     <div className=' rounded-md bg-main mb-[20px] w-[530px] h-[180px] px-[31px] py-[31px]'>
-//                         <h1 className=' text-white w-[400px] h-[56px] font-clash font-semibold text-[28px] leading-[28px] mb-2'> The Best Financial Accounting App Ever!</h1>
-//                         <p className=' w-[466px] h-[56px] font-clash font-normal text-[18px] leading-[28px] text-white text-justify'>"Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".</p>
-//                     </div>
-//                     <div>
-//                         <div className=' mb-[20px] w-[530px] h-[180px] px-[31px] py-[31px]'>
-//                               <h1 className='  w-[400px] h-[56px] font-clash font-semibold text-[28px] leading-[28px] mb-2'> The Best Financial Accounting App Ever!</h1>
-//                         <p className=' w-[466px] h-[56px] font-clash font-normal text-[18px] leading-[28px] text-justify'>"Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris".</p>
-//                         </div>
-                  
-//                     </div>
-//             </div>
-//     </div>
   )
 }
 
--- src/app/components/Reviews.jsx
+++ src/app/components/Reviews.jsx
@@ -9,10 +9,10 @@ const Reviews = () => {
     <div className='mt-8 lg:mt-0 lg:ml-8'>
         <h1 className='font-clash font-semibold leading-[28px] text-[28px] 2xl:w-[442px] 2xl:h-[56px] lg:w-[442px] lg:h-[56px] mb-[24px] ml-5'>The Best Financial Accounting App Ever!</h1>
         <p className='font-clash font-medium text-lg leading-[28px] opacity-50 2xl:w-[442px] lg:w-[442px] 2xl:h-[196px] lg:h-[196px] ml-[20px] ml-5'>
-       " Arcu At Dictum Sapien, Mollis. Vulputate Sit id Accumsan, Ultricies. In Ultrices Malesuada elit Mauris Etiam Odio. Duis tristique Lacus, Et Blandit Viverra Nisl Velit. Sed Mattis Rhoncus, Diam Suspendisse Sit Nunc, Gravida Eu. Lectus Eget Eget Ac Dolor Neque Lorem Sapien, Suspendisse Aliquam."
+       Arcu At Dictum Sapien, Mollis. Vulputate Sit id Accumsan, Ultricies. In Ultrices Malesuada elit Mauris Etiam Odio. Duis tristique Lacus, Et Blandit Viverra Nisl Velit. Sed Mattis Rhoncus, Diam Suspendisse Sit Nunc, Gravida Eu. Lectus Eget Eget Ac Dolor Neque Lorem Sapien, Suspendisse Aliquam.
       </p>
-      <Image className="mb-[24px] ml-5" src="/img-7.svg" width={192} height={42} alt="img"/>
-      <p className=" font-clash font-semibold text-[18px] leading-[28px]  ml-5">Nick Jonas</p>
+      <Image className='mb-[24px] ml-5' src='/img-7.svg' width={192} height={42} alt='img'/>
+      <p className='font-clash font-semibold text-[18px] leading-[28px]  ml-5'>Nick Jonas</p>
       </div>
       
     </div>
